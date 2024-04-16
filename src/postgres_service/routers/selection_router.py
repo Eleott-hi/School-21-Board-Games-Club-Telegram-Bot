c2163e2e03ef3a299720e648db48157154d84bd0
Curse Bowman <cursebow@in-g3.nsk.21-school.ru>
2024-04-16 15:54:47 +0700

    testing of google sync

--- src/postgres_service/routers/selection_router.py
+++ src/postgres_service/routers/selection_router.py
@@ -1,11 +1,14 @@
 from uuid import UUID
+from typing import Annotated
 from datetime import datetime, timedelta, UTC
 from typing import List
-from fastapi import APIRouter, status, HTTPException, Depends
+from fastapi import APIRouter, status, HTTPException, Depends, Query, Path
 
 router = APIRouter(prefix="/db", tags=["test"])
 
 
 @router.get("/{id}", status_code=200)
-async def test(id: int, test_int: int = None):
+async def test(id: Annotated[int, Path(title = "test id")], test_int: Annotated[int, Query()] = None):
     return {"message": id + test_int} if test_int else {"message": id}
+
+
