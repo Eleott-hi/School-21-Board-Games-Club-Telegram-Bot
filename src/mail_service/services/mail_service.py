1eed6f4f4ee34c24ee8a57d3241212357c249105
Eleott-hi <laptev18vv@gmail.com>
2024-05-19 04:03:08 +0700

    fix: mail_service is alive!!!

--- src/mail_service/services/mail_service.py
+++ src/mail_service/services/mail_service.py
@@ -1,4 +1,5 @@
 import aiosmtplib as smtp
+import ssl
 
 from email.message import EmailMessage
 from config import MAIL_PASSWORD, MAIL_ADDRESS
@@ -6,19 +7,40 @@ from schemas.schemas import EmailInfo
 
 
 class MailService:
+    def __init__(self):
+        self.smtp = "smtp.mail.ru"
 
     async def send_mail(self, email_info: EmailInfo) -> None:
+        """
+        Sends an email using SMTP.
+
+        This function sends an email to the recipient specified in the EmailInfo object.
+        Note: This function does not work when connected through a VPN.
+
+        Args:
+            email_info (EmailInfo): An object containing the email recipient's address,
+                                    subject, and message body.
+
+        Raises:
+            SMTPException: If there is an error sending the email.
+
+        Example:
+            email_info = EmailInfo(
+                email="recipient@example.com",
+                subject="Test Subject",
+                message="This is a test message."
+            )
+            await self.send_mail(email_info)
+        """
         email = EmailMessage()
         email["From"] = MAIL_ADDRESS
         email["To"] = email_info.email
         email["Subject"] = email_info.subject
         email.set_content(email_info.message)
 
-        print(email)
-
         tmp = await smtp.send(
             email,
-            hostname="smtp.mail.ru",
+            hostname=self.smtp,
             port=465,
             use_tls=True,
             username=MAIL_ADDRESS,
