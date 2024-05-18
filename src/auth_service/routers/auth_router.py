658c576116a581541fcb04bb88c03a463bd69180
Eleott-hi <laptev18vv@gmail.com>
2024-05-19 05:03:11 +0700

    fix: mail_service is alive!!!

--- src/auth_service/routers/auth_router.py
+++ src/auth_service/routers/auth_router.py
@@ -1,8 +1,6 @@
-import random
-from fastapi import APIRouter, Body, Depends, HTTPException
+from fastapi import APIRouter, Depends, status
 from schemas.auth_schemas import ConfirmSchema, RegisterSchema, User
 from services.auth_service import AuthService
-from repositories.auth_repository import AuthRepository
 
 router = APIRouter(
     prefix="/auth",
@@ -11,7 +9,10 @@ router = APIRouter(
 )
 
 
-@router.post("/register")
+@router.post(
+    "/register",
+    status_code=status.HTTP_202_ACCEPTED,
+)
 async def register(
     register_schema: RegisterSchema,
     auth_service: AuthService = Depends(),
@@ -20,7 +21,10 @@ async def register(
     await auth_service.start_registeration(register_schema)
 
 
-@router.post("/confirm_token")
+@router.post(
+    "/confirm_token",
+    status_code=status.HTTP_201_CREATED,
+)
 async def confirm_token(
     confirm_schema: ConfirmSchema,
     auth_service: AuthService = Depends(),
@@ -28,54 +32,12 @@ async def confirm_token(
     await auth_service.confirm_token(confirm_schema)
 
 
-@router.get("/user/{telegram_id}")
+@router.get(
+    "/user/{telegram_id}",
+    status_code=status.HTTP_200_OK,
+)
 async def get_user(
     telegram_id: int,
     auth_service: AuthService = Depends(),
 ) -> User:
     return await auth_service.get_user_by_telegram_id(telegram_id)
-
-
-# @router.get("/test", tags=["test"])
-# async def root():
-#     return "Hello, from auth!"
-
-
-# @router.post("/register", tags=["auth", "register"])
-# async def register():
-#     return
-
-
-# @router.post("/login", tags=["auth", "login"])
-# async def login():
-#     return
-
-
-# @router.post("/logout", tags=["auth", "logout"])
-# async def logout():
-#     return
-
-
-# @router.post("/refresh", tags=["auth", "refresh"])
-# async def refresh():
-#     return
-
-
-# @router.post("/forgot-password", tags=["auth", "forgot-password"])
-# async def forgot_password():
-#     return
-
-# @router.post("/change-password", tags=["auth", "change-password"])
-# async def change_password():
-#     return
-
-# @router.get(
-#     "/user",
-#     tags=["auth", "user"],
-#     responses={401: {"description": "Not authorized"}},
-# )
-# async def get_user():
-#     if random.choice([True, False]):
-#         raise HTTPException(status_code=401, detail="Not authorized")
-
-#     return dict(name="some name")
